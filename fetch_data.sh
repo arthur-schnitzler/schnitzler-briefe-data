350677e3de37a0715330a4327e077ba2d9115e12
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-12 11:07:57 +0200

    will it work???

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,4 +1,17 @@
 # bin/bash
-rm master.zip
-rm -rf data
-cp https://github.com/arthur-schnitzler-arbeit/trunk/editions ./data
+
+rm -rf ./data/editions
+mkdir -p ./data/editions
+rm -rf ./data/indices
+mkdir -p ./data/indices
+
+rm main.zip
+
+wget https://github.com/arthur-schnitzler/arthur-schnitzler-arbeit/archive/refs/heads/main.zip
+unzip main
+rm main.zip
+
+mv ./arthur-schnitzler-arbeit-main/editions ./data/editions
+mv ./arthur-schnitzler-arbeit-main/indices ./data/indices
+
+rm -rf ./arthur-schnitzler-arbeit-main

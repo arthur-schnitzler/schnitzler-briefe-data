1c13f738890bf4799b112f49040e876de92f8bd1
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-12 12:04:06 +0200

    now some xslts

--- fetch_data.sh
+++ fetch_data.sh
@@ -10,8 +10,9 @@ rm main.zip
 wget https://github.com/arthur-schnitzler/arthur-schnitzler-arbeit/archive/refs/heads/main.zip
 unzip main
 rm main.zip
-
-mv ./arthur-schnitzler-arbeit-main/editions ./data/editions
-mv ./arthur-schnitzler-arbeit-main/indices ./data/indices
-
+mv ./arthur-schnitzler-arbeit-main/editions ./data
+mv ./arthur-schnitzler-arbeit-main/indices ./data
 rm -rf ./arthur-schnitzler-arbeit-main
+
+echo "and now some XSLTs"
+ant

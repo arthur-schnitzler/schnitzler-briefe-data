d554a9965f439515e5eda30ecae5a51203ae5d27
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2022-10-13 12:48:45 +0200

    Update fetch_data.sh

--- fetch_data.sh
+++ fetch_data.sh
@@ -4,6 +4,8 @@ rm -rf ./data/editions
 mkdir -p ./data/editions
 rm -rf ./data/indices
 mkdir -p ./data/indices
+rm -rf ./data/meta
+mkdir -p ./data/meta
 
 rm main.zip
 
@@ -12,6 +14,8 @@ unzip main
 rm main.zip
 mv ./arthur-schnitzler-arbeit-main/editions ./data
 mv ./arthur-schnitzler-arbeit-main/indices ./data
+mv ./arthur-schnitzler-arbeit-main/meta ./data
+
 rm -rf ./arthur-schnitzler-arbeit-main
 
 echo "and now some XSLTs"

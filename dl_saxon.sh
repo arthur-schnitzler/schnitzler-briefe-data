9ed751e1fd6693256ec630e979a8af4a07c118f9
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-11-10 09:16:09 +0100

    dl von maven central

--- dl_saxon.sh
+++ dl_saxon.sh
@@ -1,2 +1,2 @@
-echo "downloading saxon"
-wget https://sourceforge.net/projects/saxon/files/Saxon-HE/9.9/SaxonHE9-9-1-7J.zip/download && unzip download -d saxon && rm -rf download
+echo "downloading saxon from maven central"
+wget https://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/9.9.1-7/Saxon-HE-9.9.1-7.jar -O saxon.jar && mkdir -p saxon && mv saxon.jar saxon/

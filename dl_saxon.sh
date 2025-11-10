b9fc467ae603a27ad87181676904a813dc4dfa62
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-11-10 09:32:45 +0100

    dl_saxon.sh weg

--- dl_saxon.sh
+++
@@ -1,2 +0,0 @@
-echo "downloading saxon from maven central"
-wget https://repo1.maven.org/maven2/net/sf/saxon/Saxon-HE/9.9.1-7/Saxon-HE-9.9.1-7.jar -O saxon.jar && mkdir -p saxon && mv saxon.jar saxon/
